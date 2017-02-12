8467fa0635d76d155b5dd293258cdd8e7bb61ae3
aleisa <ysen6636068@live.com>
2017-02-12 18:59:16 +0800

    Summary
    帅哥

--- src/main/java/com/eminem/controller/BussinessController.java
+++ src/main/java/com/eminem/controller/BussinessController.java
@@ -1,11 +1,13 @@
 package com.eminem.controller;
 
 import com.eminem.service.BussinessService;
+import com.eminem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,11 +19,16 @@ import java.util.Map;
 public class BussinessController {
     @Autowired
     BussinessService bussinessService;
-
+    @Autowired
+    UserService userService;
     @RequestMapping
     public List<Map<String,Object>> findAll(){
         List<Map<String,Object>> all=new ArrayList<Map<String, Object>>();
         all=bussinessService.selectAll();
+        Map<String,Object> user =new HashMap<>();
+        user.put("username","wang");
+        user.put("password","123");
+        userService.createUser(user);
         return all;
     }
 }
