f6f8f5c153bf5ac3b8f65eec1890b60eb1554163
aleisa <ysen6636068@live.com>
2017-03-11 12:29:31 +0800

    nice

--- src/main/java/com/eminem/util/RedisUtil.java
+++ src/main/java/com/eminem/util/RedisUtil.java
@@ -1,12 +1,28 @@
 package com.eminem.util;
 
+import com.eminem.entity.User;
+import com.eminem.service.intf.UserRepository;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,10 +30,15 @@ import java.util.concurrent.TimeUnit;
  */
 @SuppressWarnings("unchecked")
 @Component
-public class RedisUtil {
+@EnableScheduling
+public  class RedisUtil {
     @SuppressWarnings("rawtypes")
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserRepository repository;
+
     /**
      * 批量删除对应的value
      *
@@ -87,6 +108,8 @@ public class RedisUtil {
         }
         return result;
     }
+
+
     /**
      * 写入缓存
      *
@@ -106,4 +129,45 @@ public class RedisUtil {
         }
         return result;
     }
+
+    /**
+     * 发送消息队列
+     * @param topic 事件类型
+     * @param event 事件参数
+     * @return
+     */
+    public boolean leftPush(String topic,Object event){
+        try {
+            redisTemplate.opsForList().leftPush(topic,event);
+        }catch (Exception e ){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+    @Scheduled(fixedRate = 10000)
+    public void rightPop(){
+        try {
+           Long size =  redisTemplate.opsForList().size("1");
+           if(size<=0){
+               return;
+           }
+           for(int i=0;i<size;i++){
+               User user =(User) redisTemplate.opsForList().rightPop("1");
+               repository.save(user);
+           }
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+
+
 }
